71227e5416d330ae7b542cee0d9aa85e0ba9110a
Vic <54647425+vicglad@users.noreply.github.com>
2026-05-27 13:02:04 +0100

    Updated deadlines

--- deadlines.js
+++ deadlines.js
@@ -1,10 +1,3 @@
 window.DEADLINES = {
-  "deadlines": [
-          {
-            name: "FEEG2002 Exam",
-            label: "Mechanics, Machines, and Vibrations",
-            datetime: "2026-05-26T09:00:00",
-            start: "2026-01-29T00:00:00"
-          }
-  ]
+  "deadlines": []
 };
